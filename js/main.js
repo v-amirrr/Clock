8c04bb32d8b0f9e81b42168850c13b295e1747ef
AmirMohammad Valizadeh <amirrr.valizadeh@gmail.com>
2021-09-13 20:41:40 +0430

    built clock page and added the analog and digital switcher

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@ function load() {
 }
 
 
+
+
+// ! footer
+const date = new Date();
+document.querySelector("footer").innerHTML = `${date.getFullYear()} &#169; Copyright`;
+
+
+
+
 // ! dark and light switch button
 const modeSwitch = document.querySelector("#mode-switch");
 const modeSwitchIcon = document.querySelector("#mode-switch span");
@@ -37,4 +46,37 @@ modeSwitch.addEventListener("click", () => {
         mode = "dark";
 
     }
+});
+
+
+
+
+// ! clock switch
+const analogSwitch = document.querySelector("#analog-switch");
+const digitalSwitch = document.querySelector("#digital-switch");
+
+const analogClock = document.querySelector("#analog-clock");
+const digitalClock = document.querySelector("#digital-clock");
+
+analogSwitch.addEventListener("click", () => {
+    analogSwitch.classList.add("switch-select");
+    digitalSwitch.classList.remove("switch-select");
+    
+    digitalClock.style.transform = "translateY(5rem)";
+    digitalClock.style.opacity = 0;
+
+    analogClock.style.transform = "translateY(0)";
+    analogClock.style.opacity = 1;
+
+});
+
+digitalSwitch.addEventListener("click", () => {
+    analogSwitch.classList.remove("switch-select");
+    digitalSwitch.classList.add("switch-select");
+
+    analogClock.style.transform = "translateY(5rem)";
+    analogClock.style.opacity = 0;
+
+    digitalClock.style.transform = "translateY(0)";
+    digitalClock.style.opacity = 1;
 });
