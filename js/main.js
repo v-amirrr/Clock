4bd88cd7ed108c008c9db560b65912ba5de9e32c
AmirMohammad Valizadeh <amirrr.valizadeh@gmail.com>
2021-09-27 20:55:42 +0330

    built stopwatch page

--- js/main.js
+++ js/main.js
@@ -147,3 +147,182 @@ function setDate(){
 }
 
 setInterval(setDate, 1000);
+
+
+
+
+// ! pages
+const clockPage = document.querySelector("#clock-page");
+const stopwatchPage = document.querySelector("#stopwatch-page");
+const timerPage = document.querySelector("#timer-page");
+
+const clockBtn = document.querySelector("#clock-btn");
+const stopwatchBtn = document.querySelector("#stopwatch-btn");
+const timerBtn = document.querySelector("#timer-btn");
+
+stopwatchBtn.addEventListener("click", () => {
+    clockBtn.classList.remove("select");
+    stopwatchBtn.classList.add("select");
+    timerBtn.classList.remove("select");
+
+    clockPage.style.transform = "translateX(-5rem)";
+    clockPage.style.opacity = 0;
+    clockPage.style.zIndex = "-2";
+
+    stopwatchPage.style.transform = "translateX(0)";
+    stopwatchPage.style.opacity = 1;
+    stopwatchPage.style.zIndex = "2";
+
+    timerPage.style.transform = "translateX(5rem)";
+    timerPage.style.opacity = 0;
+    timerPage.style.zIndex = "-2";
+});
+
+
+timerBtn.addEventListener("click", () => {
+    clockBtn.classList.remove("select");
+    stopwatchBtn.classList.remove("select");
+    timerBtn.classList.add("select");
+    
+    clockPage.style.transform = "translateX(-5rem)";
+    clockPage.style.opacity = 0;
+    clockPage.style.zIndex = "-2";
+
+    stopwatchPage.style.transform = "translateX(-5rem)";
+    stopwatchPage.style.opacity = 0;
+    stopwatchPage.style.zIndex = "-2";
+
+    timerPage.style.transform = "translateX(0)";
+    timerPage.style.opacity = 1;
+    timerPage.style.zIndex = "2";
+});
+
+
+clockBtn.addEventListener("click", () => {
+    clockBtn.classList.add("select");
+    stopwatchBtn.classList.remove("select");
+    timerBtn.classList.remove("select");
+
+    clockPage.style.transform = "translateX(0)";
+    clockPage.style.opacity = 1;
+    clockPage.style.zIndex = "2";
+
+    stopwatchPage.style.transform = "translateX(5rem)";
+    stopwatchPage.style.opacity = 0;
+    stopwatchPage.style.zIndex = "-2";
+
+    timerPage.style.transform = "translateX(5rem)";
+    timerPage.style.opacity = 0;
+    timerPage.style.zIndex = "-2";
+});
+
+
+
+
+// ! stopwatch
+const stopwatchBtn1 = document.querySelector("#stopwatch-btn1");
+const stopwatchBtn1P = document.querySelector("#stopwatch-btn1 p");
+const stopwatchBtn2 = document.querySelector("#stopwatch-btn2");
+
+stopwatchBtn1.addEventListener("click", () => {
+    if(stopwatchBtn1P.innerText == "Start"){        
+        stopwatchBtn2.style.opacity = 1;
+        stopwatchBtn2.style.transform = "translate(0)";
+    
+        changeButtonText("Stop");
+
+        stopwatchStart();
+    }
+
+    if(stopwatchBtn1P.innerText == "Stop"){
+        changeButtonText("Resume");
+
+        stopwatchStop();
+    }
+
+    if(stopwatchBtn1P.innerText == "Resume"){
+        changeButtonText("Stop");
+
+        stopwatchStart();
+    }
+});
+
+stopwatchBtn2.addEventListener("click", () => {
+    stopwatchBtn2.style.transform = "translate(2rem)";
+    stopwatchBtn2.style.opacity = 0;
+
+    changeButtonText("Start");
+
+    stopwatchReset();
+});
+
+function changeButtonText(text){
+    stopwatchBtn1P.style.transform = "translateX(-5rem)";
+    stopwatchBtn1P.style.opacity = 0;
+
+    setTimeout(() => {
+        stopwatchBtn1P.innerHTML = text;
+        stopwatchBtn1P.style.transition = "none";
+        stopwatchBtn1P.style.transform = "translateX(5rem)";
+        setTimeout(() => {
+            stopwatchBtn1P.style.transition = "all .5s";
+            stopwatchBtn1P.style.transform = "translateX(0)";
+            stopwatchBtn1P.style.opacity = 1;
+        }, 50); 
+    }, 500);
+    
+}
+
+const stopwatchH = document.querySelector("#stopwatch-h");
+const stopwatchM = document.querySelector("#stopwatch-m");
+const stopwatchS = document.querySelector("#stopwatch-s");
+
+let stopwatchHour = 0;
+let stopwatchMinute = 0;
+let stopwatchSecond = 0;
+let stopwatch;
+
+function lessThan10(val, elm){
+    if(val < 10){
+        elm.innerHTML = `0${val}`;
+    }else{
+        elm.innerHTML = val;
+    }
+}
+
+function stopwatchStart(){
+    document.querySelector("#stopwatch").style.animationPlayState = "running";
+
+    stopwatch = setInterval(() => {
+
+        if(stopwatchSecond < 60){
+            stopwatchSecond += 1;
+            lessThan10(stopwatchSecond, stopwatchS);
+        }else{
+            stopwatchSecond = 0;
+            if(stopwatchMinute < 60){
+                stopwatchMinute += 1;
+                lessThan10(stopwatchMinute, stopwatchM);
+            }else{
+                stopwatchHour += 1;
+                lessThan10(stopwatchHour, stopwatchH);
+            }
+        }
+
+    }, 1000);
+}
+
+function stopwatchStop(){
+    clearInterval(stopwatch);
+    document.querySelector("#stopwatch").style.animationPlayState = "paused";
+}
+
+function stopwatchReset(){
+    stopwatchStop();
+    stopwatchHour = 0;
+    stopwatchMinute = 0;
+    stopwatchSecond = 0;
+    stopwatchH.innerHTML = "00";
+    stopwatchM.innerHTML = "00";
+    stopwatchS.innerHTML = "00";
+}
