275ef041499abf3bcbaef7d1ac20f430746ca965
AmirMohammad Valizadeh <amirrr.valizadeh@gmail.com>
2021-10-10 17:48:44 +0330

    built timer page

--- js/main.js
+++ js/main.js
@@ -269,8 +269,7 @@ function changeButtonText(text){
             stopwatchBtn1P.style.transform = "translateX(0)";
             stopwatchBtn1P.style.opacity = 1;
         }, 50); 
-    }, 500);
-    
+    }, 500);   
 }
 
 const stopwatchH = document.querySelector("#stopwatch-h");
@@ -325,4 +324,283 @@ function stopwatchReset(){
     stopwatchH.innerHTML = "00";
     stopwatchM.innerHTML = "00";
     stopwatchS.innerHTML = "00";
+}
+
+
+
+
+// ! timer
+const timerBtn1 = document.querySelector("#timer-btn1");
+const timerBtn1P = document.querySelector("#timer-btn1 p");
+const timerBtn2 = document.querySelector("#timer-btn2");
+
+const timerButtons = document.querySelector("#timer-buttons");
+const timerLine = document.querySelector("#timer-line");
+
+const timerInputDiv = document.querySelectorAll("#timer-input div");
+const timerInputHr = document.querySelectorAll("#timer-input hr");
+
+const timerInputHour = document.querySelector("#hour input");
+const timerInputMinute = document.querySelector("#minute input");
+const timerInputSecond = document.querySelector("#second input");
+
+let hour = document.querySelector("#hour input");
+let minute = document.querySelector("#minute input");
+let second = document.querySelector("#second input");
+
+const lineCopy = document.querySelector("#timer-line-copy");
+
+let hourValue;
+let minuteValue;
+let secondValue;
+
+let timer;
+
+let timerCheck = /^([0-5]?[0-9]|60)$/;
+
+timerBtn1.addEventListener("click", () => {
+    let time = `${hour.value}${minute.value}${second.value}`;
+
+    if(timerBtn1P.innerText == "Start" && time != 0 && timerCheck.test(minute.value) && timerCheck.test(second.value) && timerCheck.test(hour.value)){        
+        timerBtn2.style.opacity = 1;
+        timerBtn2.style.transform = "translate(0)";
+    
+        changeTimerButtonText("Stop");
+
+        timerButtons.style.bottom = "2rem";
+
+        setTimeout(() => {
+            timerLine.style.bottom = ".5rem";
+            timerLine.style.opacity = 1;
+
+            timerInputDiv.forEach(div => {
+                div.style.margin = 0;
+            });
+
+            timerInputHour.style.borderRadius = "25px 0 0 25px";
+            timerInputMinute.style.borderRadius = 0;
+            timerInputSecond.style.borderRadius = "0 25px 25px 0";
+
+            timerInputHour.disabled = true;
+            timerInputMinute.disabled = true;
+            timerInputSecond.disabled = true;
+
+
+        }, 500);
+
+        startTimer();
+    } else if (!(timerCheck.test(minute.value) && timerCheck.test(second.value) && timerCheck.test(hour.value))){
+        timerError();
+    }
+
+    if(timerBtn1P.innerText == "Stop"){
+        changeTimerButtonText("Resume");
+        stopTimer();
+    }
+
+    if(timerBtn1P.innerText == "Resume"){
+        changeTimerButtonText("Stop");
+        resumeTimer();
+    }
+});
+
+timerBtn2.addEventListener("click", () => {
+    timerBtn2.style.transform = "translate(2rem)";
+    timerBtn2.style.opacity = 0;
+
+    changeTimerButtonText("Start");
+
+    timerLine.style.bottom = "5rem";
+    timerLine.style.opacity = 0;
+
+    timerButtons.style.bottom = "5rem";
+
+    timerInputDiv.forEach(div => {
+        div.style.margin = "1rem";
+    });
+
+    timerInputHour.style.borderRadius = "25px";
+    timerInputMinute.style.borderRadius = "25px";
+    timerInputSecond.style.borderRadius = "25px";
+
+    timerInputHour.disabled = false;
+    timerInputMinute.disabled = false;
+    timerInputSecond.disabled = false;
+
+    resetTimer();
+});
+
+function changeTimerButtonText(text){
+    timerBtn1P.style.transform = "translateX(-5rem)";
+    timerBtn1P.style.opacity = 0;
+
+    setTimeout(() => {
+        timerBtn1P.innerHTML = text;
+        timerBtn1P.style.transition = "none";
+        timerBtn1P.style.transform = "translateX(5rem)";
+        setTimeout(() => {
+            timerBtn1P.style.transition = "all .5s";
+            timerBtn1P.style.transform = "translateX(0)";
+            timerBtn1P.style.opacity = 1;
+        }, 50); 
+    }, 500);   
+}
+
+
+function startTimer(){
+    hourValue = parseInt(hour.value);
+    minuteValue = (parseInt(hour.value) * 60) + parseInt(minute.value);
+    secondValue = (parseInt(minuteValue) * 60) + parseInt(second.value);
+
+    let distance = (15 / secondValue);
+    let line = 15;
+    
+    if(hour.value <= 10 && hour.value != 00){
+        hour.value = `0${hour.value}`;
+    }
+    
+    if(minute.value <= 10 && minute.value != 00){
+        minute.value = `0${minute.value}`;
+    }
+    
+    
+    timer = setInterval(() => {
+        line = line - distance;
+        line.toFixed(2);
+        lineCopy.style.transform = `translateX(-${line}rem)`;
+
+        if(secondValue > 0){
+            
+            if(parseInt(second.value) > 0){
+                
+                if(second.value <= 10){
+                    second.value = `0${second.value - 1}`;
+                } else if (second.value > 10){
+                    second.value = second.value - 1;
+                }
+
+            }else if(parseInt(second.value) == 0){
+
+                if(secondValue >= 60){
+
+                    second.value = 59;
+
+                    if(parseInt(minute.value) > 0){
+
+                        minuteValue--;
+
+                        if(minute.value <= 10){
+                            minute.value = `0${minute.value - 1}`;
+                        } else if (minute.value > 10){
+                            minute.value = minute.value - 1;
+                        }
+
+                    }else if(parseInt(minute.value) <= 0){
+                        if(minuteValue >= 60){
+
+                            minuteValue = minuteValue - 60;
+                            minute.value = 59;
+
+                            if(parseInt(hourValue) != 0 && parseInt(hour.value) != 0){
+
+                                hourValue--;
+        
+                                if(hour.value <= 10){
+                                    hour.value = `0${hour.value - 1}`;
+                                } else if (hour.value > 10){
+                                    hour.value = hour.value - 1;
+                                }
+                            }
+
+                        } else if (minuteValue < 60){
+
+                            minute.value = minuteValue;
+
+                        }
+                    }
+
+                } else if(secondValue < 60){
+                    second.value = secondValue;
+                }
+
+            }
+
+            secondValue--;
+
+        }else if (secondValue <= 0){
+            timerFinish();
+        }
+        
+    }, 1000);
+}
+
+function stopTimer(){
+    clearInterval(timer);
+}
+
+function resumeTimer(){
+    startTimer();
+}
+
+function resetTimer(){
+    second.value = "00";
+    minute.value = "00";
+    hour.value = "00";
+
+    clearInterval(timer);
+
+    lineCopy.style.transform = "translateX(-15rem)";
+}
+
+let audio = new Audio("./audio/timer-sound.mp3");
+
+function timerFinish(){
+    audio.play();
+
+    second.value = "00";
+    minute.value = "00";
+    hour.value = "00";
+
+    clearInterval(timer);
+
+    document.querySelector("#timer-window").style.zIndex = 999;
+    document.querySelector("#timer-window").style.transform = "scale(1)";
+
+    lineCopy.style.transform = "translateX(-15rem)";
+}
+
+document.querySelector("#timer-window-btn").addEventListener("click", () => {
+    audio.pause();
+
+    document.querySelector("#timer-window").style.transform = "scale(0)";
+    setTimeout(() => {
+        document.querySelector("#timer-window").style.zIndex = 999;
+    }, 500);
+
+    timerBtn2.style.transform = "translate(2rem)";
+    timerBtn2.style.opacity = 0;
+
+    changeTimerButtonText("Start");
+
+    timerLine.style.bottom = "5rem";
+    timerLine.style.opacity = 0;
+
+    timerButtons.style.bottom = "5rem";
+
+    timerInputDiv.forEach(div => {
+        div.style.margin = "1rem";
+    });
+
+    timerInputHour.style.borderRadius = "25px";
+    timerInputMinute.style.borderRadius = "25px";
+    timerInputSecond.style.borderRadius = "25px";
+
+    timerInputHour.disabled = false;
+    timerInputMinute.disabled = false;
+    timerInputSecond.disabled = false;
+});
+
+function timerError(){
+    alert("You can only enter tow-digit integer numbers between 0 and 60.");
+    resetTimer();
 }
